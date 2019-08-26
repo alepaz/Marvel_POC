b947384f31a64f7272fc3551835580835eb51b21
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-26 00:19:03 -0600

    Import axios and set correct action type name

--- marvel_poc_ui/src/actions/index.js
+++ marvel_poc_ui/src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
     FETCH_HEROES,
     FETCH_HEROE
--- marvel_poc_ui/src/actions/types.js
+++ marvel_poc_ui/src/actions/types.js
@@ -1,2 +1,2 @@
-export const FETCH_HEROES = 'fetch_employees';
-export const FETCH_HEROE = 'fetch_employee';
+export const FETCH_HEROES = 'fetch_heroes';
+export const FETCH_HEROE = 'fetch_hero';
