91e855e5c21a42e1ddc3cea68bcfe5c525c5de66
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-26 00:19:47 -0600

    Connect to the storage and render heroes

--- marvel_poc_ui/src/components/Landing.js
+++ marvel_poc_ui/src/components/Landing.js
@@ -1,107 +1,54 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchHeroes } from '../actions';
 
 class Landing extends Component {
-  // componentDidMount() {
-  //     this.props.fetchMarvelData();
-  // }
+  componentDidMount() {
+      this.props.fetchHeroes();
+  }
+
+  renderHeroes() {
+    console.log(this.props.heroes);
+    try{
+      return this.props.heroes.data.results.map( heroe => {
+        return (
+          <div className="card col s3">
+            <div className="card-image waves-effect waves-block waves-light">
+              <img className="activator" src={heroe.thumbnail.path+"."+heroe.thumbnail.extension} />
+            </div>
+            <div className="card-content">
+              <span className="card-title activator grey-text text-darken-4">
+                {heroe.name}<i className="material-icons right">more_vert</i>
+              </span>
+            </div>
+            <div className="card-reveal">
+              <span className="card-title grey-text text-darken-4">
+              {heroe.name}<i className="material-icons right">close</i>
+              </span>
+              <p>
+                { heroe.description ? heroe.description : "No description available" }
+              </p>
+            </div>
+          </div>
+        );
+      });
+    }catch(err){
+      return '';
+    }
+
+  }
 
   render() {
     return (
       <div className="row">
-        <div className="card col s3">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src="http://materializecss.com/images/office.jpg" />
-          </div>
-          <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">
-              Card Title<i className="material-icons right">more_vert</i>
-            </span>
-            <p>
-              <a href="#">This is a link</a>
-            </p>
-          </div>
-          <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">
-              Card Title<i className="material-icons right">close</i>
-            </span>
-            <p>
-              Here is some more information about this product that is only
-              revealed once clicked on.
-            </p>
-          </div>
-        </div>
-
-        <div className="card col s3">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src="http://materializecss.com/images/office.jpg" />
-          </div>
-          <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">
-              Card Title<i className="material-icons right">more_vert</i>
-            </span>
-            <p>
-              <a href="#">This is a link</a>
-            </p>
-          </div>
-          <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">
-              Card Title<i className="material-icons right">close</i>
-            </span>
-            <p>
-              Here is some more information about this product that is only
-              revealed once clicked on.
-            </p>
-          </div>
-        </div>
-
-        <div className="card col s3">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src="http://materializecss.com/images/office.jpg" />
-          </div>
-          <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">
-              Card Title<i className="material-icons right">more_vert</i>
-            </span>
-            <p>
-              <a href="#">This is a link</a>
-            </p>
-          </div>
-          <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">
-              Card Title<i className="material-icons right">close</i>
-            </span>
-            <p>
-              Here is some more information about this product that is only
-              revealed once clicked on.
-            </p>
-          </div>
-        </div>
-
-        <div className="card col s3">
-          <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src="http://materializecss.com/images/office.jpg" />
-          </div>
-          <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">
-              Card Title<i className="material-icons right">more_vert</i>
-            </span>
-            <p>
-              <a href="#">This is a link</a>
-            </p>
-          </div>
-          <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">
-              Card Title<i className="material-icons right">close</i>
-            </span>
-            <p>
-              Here is some more information about this product that is only
-              revealed once clicked on.
-            </p>
-          </div>
-        </div>
+        {this.renderHeroes()}
       </div>
     );
   }
 }
 
-export default Landing;
+function mapStateToProps({ heroes }) {
+  return { heroes };
+}
+
+export default connect(mapStateToProps, { fetchHeroes })(Landing);
