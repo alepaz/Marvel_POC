92524dc8a28ba0d07258bd5dbe2f645819a5fd70
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-25 23:42:35 -0600

    wrap app in provider and initial state

--- marvel_poc_ui/src/index.js
+++ marvel_poc_ui/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "materialize-css/dist/css/materialize.min.css";
-import 'material-icons/iconfont/material-icons.css';
+import "material-icons/iconfont/material-icons.css";
+import configureStore from "./helpers/configureStore";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+//Reducer, initial state of application, applyMiddleware
+const store = configureStore();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
