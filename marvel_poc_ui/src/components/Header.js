0584161fac2f6a35cb3f6d4cb24c52341b4dab29
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-27 00:13:09 -0600

    Display routes on the Header

--- marvel_poc_ui/src/components/Header.js
+++ marvel_poc_ui/src/components/Header.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 
 class Header extends Component {
-
-    render() {
-        return (
-            <nav>
-                <div className="nav-wrapper">
-                    <div
-                        className="left brand-logo"
-                        style={{ margin: '0 10px' }}
-                    >
-                        Marvel Test
-                    </div>
-                    <ul className="right">
-                        
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+  render() {
+    const {
+      location: { pathname }
+    } = this.props;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <div className="left brand-logo" style={{ margin: "0 10px" }}>
+            Marvel Test
+          </div>
+          <ul className="right">
+            <li className={`nav-link${pathname === "/" ? " active" : ""}`}>
+              <Link to="/">Characters</Link>
+            </li>
+            <li
+              className={`nav-link${pathname === "/comics" ? " active" : ""}`}
+            >
+              <Link to="/comics">Comics</Link>
+            </li>
+            <li
+              className={`nav-link${pathname === "/stories" ? " active" : ""}`}
+            >
+              <Link to="/stories">Stories</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+export default withRouter(props => <Header {...props} />);
