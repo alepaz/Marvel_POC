3bf005d8ab960c3e4a9ba787b71e0df37623abf4
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-27 14:20:30 -0600

    On heroku bff is going to serve static build react app

--- bff/index.js
+++ bff/index.js
@@ -9,12 +9,12 @@ require('./routes/Api')(app);
 
 //On production server static files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../marvel_poc_ui/build"));
+  app.use(express.static("./marvel_poc_ui/build"));
 
   // Express will serve up the index.html file
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "ui", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "marvel_poc_ui", "build", "index.html"));
   });
 }
 
