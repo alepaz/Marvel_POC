7c468eb5b240f2871e3def79cdfbdbb916f3b5d6
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-27 02:47:50 -0600

    Combine reducers for comicsReducer and storiesReducer

--- marvel_poc_ui/src/reducers/index.js
+++ marvel_poc_ui/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import heroesReducer from './heroesReducer';
+import comicsReducer from './comicsReducer';
+import storiesReducer from './storiesReducer';
 
 export default combineReducers({
     //Reducers are assign to keys
-    heroes: heroesReducer
+    heroes: heroesReducer,
+    comics: comicsReducer,
+    stories: storiesReducer
 });
