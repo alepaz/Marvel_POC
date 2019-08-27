66cacd650234572dedc9a32fce11918dbf3cb756
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-26 23:36:54 -0600

    Remove unnecessary files

--- marvel_poc_ui/src/index.js
+++ marvel_poc_ui/src/index.js
@@ -5,7 +5,6 @@ import "materialize-css/dist/css/materialize.min.css";
 import "material-icons/iconfont/material-icons.css";
 import configureStore from "./helpers/configureStore";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
 //Reducer, initial state of application, applyMiddleware
 const store = configureStore();
@@ -16,8 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
