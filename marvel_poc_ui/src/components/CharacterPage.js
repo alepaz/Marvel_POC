e1c8ac5d64ba95b5e660e35b735f2a8cdda78e4a
Francisco Paz <francisco.paz@telusinternational.com>
2019-08-27 00:03:52 -0600

    Create HeroCard component

--- marvel_poc_ui/src/components/CharacterPage.js
+++ marvel_poc_ui/src/components/CharacterPage.js
@@ -2,75 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchHeroes } from "../actions";
 import "../css/landing.css";
+import HeroCard from "./HeroCard";
 
 class CharacterPage extends Component {
   componentDidMount() {
     this.props.fetchHeroes();
   }
 
-  renderHeroes() {
-    try {
-      return this.props.heroes.data.results.map(heroe => {
-        return (
-          <div className="card col s6 m4" key={heroe.id}>
-            <div className="card-image waves-effect waves-block waves-light">
-              <img
-                className="activator hero-image"
-                src={heroe.thumbnail.path + "." + heroe.thumbnail.extension}
-              />
-            </div>
-            <div className="card-content">
-              <span className="activator grey-text text-darken-4">
-                {heroe.name.length > 15
-                  ? heroe.name
-                      .substring(0, 15)
-                      .split("")
-                      .concat("...")
-                      .join("")
-                  : heroe.name}
-                <i className="material-icons right">more_vert</i>
-              </span>
-            </div>
-            <div className="card-reveal">
-              <span className="card-title grey-text text-darken-4">
-                {heroe.name}
-                <i className="material-icons right">close</i>
-              </span>
-              <p>
-                {heroe.description
-                  ? heroe.description
-                  : "No description available"}
-              </p>
-
-              <span className="card-title grey-text text-darken-4">Comics</span>
-              {heroe.comics.items.map((x, i) => {
-                return (
-                  <div className="chip" key={heroe.id + "comic" + i}>
-                    {x.name}
-                  </div>
-                );
-              })}
-              <span className="card-title grey-text text-darken-4">
-                Stories
-              </span>
-              {heroe.stories.items.map((x, i) => {
-                return (
-                  <div className="chip" key={heroe.id + "story" + i}>
-                    {x.name}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        );
-      });
-    } catch (err) {
-      return ;
-    }
-  }
-
   render() {
-    return <div className="row">{this.renderHeroes()}</div>;
+    const { heroes } = this.props;
+    return (
+      <div className="row">
+        {heroes && heroes.data && heroes.data.results ? (
+          heroes.data.results.map(hero => <HeroCard {...hero} />)
+        ) : (
+          <p>There aren't any character to display</p>
+        )}
+      </div>
+    );
   }
 }
 
---
+++ marvel_poc_ui/src/components/HeroCard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function HeroCard({ id, comics, thumbnail, name, description, stories }) {
+  console.log("rendering Hero", name);
+  return (
+    <div className="card col s6 m4" key={id}>
+      <div className="card-image waves-effect waves-block waves-light">
+        <img
+          className="activator hero-image"
+          src={thumbnail.path + "." + thumbnail.extension}
+        />
+      </div>
+      <div className="card-content">
+        <span className="activator grey-text text-darken-4">
+          {name.length > 15
+            ? name
+                .substring(0, 15)
+                .split("")
+                .concat("...")
+                .join("")
+            : name}
+          <i className="material-icons right">more_vert</i>
+        </span>
+      </div>
+      <div className="card-reveal">
+        <span className="card-title grey-text text-darken-4">
+          {name}
+          <i className="material-icons right">close</i>
+        </span>
+        <p>{description ? description : "No description available"}</p>
+
+        <span className="card-title grey-text text-darken-4">Comics</span>
+        {comics.items.map((x, i) => {
+          return (
+            <div className="chip" key={id + "comic" + i}>
+              {x.name}
+            </div>
+          );
+        })}
+        <span className="card-title grey-text text-darken-4">Stories</span>
+        {stories.items.map((x, i) => {
+          return (
+            <div className="chip" key={id + "story" + i}>
+              {x.name}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+HeroCard.propTypes = {
+  comics: PropTypes.shape({
+    items: PropTypes.arrayOf(PropTypes.object)
+  }).isRequired,
+  description: PropTypes.string,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  stories: PropTypes.shape({
+    items: PropTypes.arrayOf(PropTypes.object)
+  }).isRequired,
+  thumbnail: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    extension: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default HeroCard;
